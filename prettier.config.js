56aac10e05b463e877323120400ad952f6d15cd8
Julio <julioarmando321@gmail.com>
2021-05-28 14:39:29 -0300

    feat: add 02-configurando eslint

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // Para utilizar aspas simples
+  trailingComma: false, // Para adicionar a ultima vírgula no final dos objetos e arrays
+  allowParens: 'avoid' // Para não adicionar parênteses em arrow functions que tenham apenas 1 parâmetro
+}
