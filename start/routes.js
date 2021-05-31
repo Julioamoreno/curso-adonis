833f0225c6e30194743bceae7a5d820fa1aa6c16
Julio <julioarmando321@gmail.com>
2021-05-31 15:23:10 -0300

    feat: add 17-validando rotas

--- start/routes.js
+++ start/routes.js
@@ -4,14 +4,30 @@
 /** @type {typeof import('@adonisjs/validator/src/Validator')} */
 const Route = use('Route')
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
 Route.group(() => {
   Route.get('files/:id', 'FileController.show')
   Route.post('files', 'FileController.store')
 
-  Route.post('passwords', 'ForgotPasswordController.store')
-  Route.put('passwords', 'ForgotPasswordController.update')
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+  Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map(
+      [
+        [
+          ['projects.store'],
+          ['Project']
+        ]
+      ]
+    ))
+  Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map(
+    [
+      [
+        ['projects.tasks.store'],
+        ['Task']
+      ]
+    ]
+  ))
 }).middleware(['auth'])
